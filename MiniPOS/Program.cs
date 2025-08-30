2372d64d1a7be0df5b6b09f96386ba54a7c2ed13
Lucas <lucasromero.ag@outlook.com>
2025-08-30 10:33:03 +0100

    Corrigido rastreamento de arquivos temporários e primeira versao do miniPOS concluida

--- MiniPOS/Program.cs
+++ MiniPOS/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿// Versão simples: Programa que solicita ao usuário os preços de três produtos e exibe o valor total da compra e o preço médio por item.
+
+Console.WriteLine("Informe o preço dos produtos separados por espaço.");
+string[] productPrice = Console.ReadLine().Split(' ');
+double sum = 0.0;
+
+for(int i = 0;i< productPrice.Length; i++)
+{
+    sum += Convert.ToDouble(productPrice[i]);
+}
+
+double average = sum / productPrice.Length;
+
+Console.WriteLine($"O valor total é: R$ {sum:F2}, assim, o preço médio por item é R$ {average:F2}");
